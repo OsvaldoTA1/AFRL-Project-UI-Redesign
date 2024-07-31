d7837bb8394f859a5d32055489f23d4bd670d7cf
Will0714160 <patrickyan51418@gmail.com>
2024-07-31 09:54:50 -0400

    Added configurations for session management

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_socketio import SocketIO
+from datetime import timedelta
 import cred
 
 socketio = SocketIO()
@@ -20,9 +21,19 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with secure key in production
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    
+    #Keys necessary for running the reCaptcha
     app.config['RECAPTCHA_PUBLIC_KEY'] = cred.RECAPTCHA_PUBLIC_KEY
     app.config['RECAPTCHA_PRIVATE_KEY'] = cred.RECAPTCHA_PRIVATE_KEY
 
+    app.config['REMEMBER_COOKIE_SECURE'] = True # Persistant Cookies can only be sent through secure HTTPS connections
+    app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days = 7) # Persistant cookies expires within 7 days of creation
+
+    app.config['SESSION_COOKIE_SECURE'] = True # Session Cookies can only be sent through secure HTTPS connection
+
+    # Session cookies expire after 10 minutes or if the browser is closed, but gets automatically renewed if persistant cookie is active by Flask Login 
+    # or if there is user activity
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 10) 
 
     app.static_folder = 'static'
     
